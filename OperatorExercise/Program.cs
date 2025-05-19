f0eda3721eea92582119ddc800dddf7a70d2cbc0
Jessica Cassorla <jessicacassorla@gmail.com>
2025-05-19 18:58:34 -0500

    exercise 1, created int a and int b and calculated quotient and remainder

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+            
+            int sum = a + b;
+            int subt = a - b;
+            int mult = a * b;
+
+            int quotient = a / b;
+            int remainder = a % b;
+
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}.");
         }
     }
 }
